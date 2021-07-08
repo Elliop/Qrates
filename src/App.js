fc6aeda7a0b448bca9640831fe82876ffe6c7639
Abdelmouhaymine Salah <asalah@student.1337.ma>
2021-07-08 20:09:39 +0100

    App Bar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Layout>
     </Router>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Button, makeStyles } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: "#f9f9f9",
+      width: "100%",
+      padding: theme.spacing(3),
+    },
+    root: {
+      display: "flex",
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    active: {
+      background: "#f4f4f4",
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+    appBar: {
+      width: "100%",
+      marginLeft: drawerWidth,
+    },
+    date: {
+      flexGrow: 1,
+    },
+    toolbar: theme.mixins.toolbar,
+    avatar: {
+      marginLeft: theme.spacing(2),
+    },
+  };
+});
+
+export default function Layout({ children }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {/* app bar */}
+      <AppBar
+        position="fixed"
+        className={classes.appBar}
+        elevation={0}
+        color="primary"
+      >
+        <Toolbar>
+          <Typography className={classes.date}>My Logo</Typography>
+          <Button>Register</Button>
+          <Button>Login</Button>
+        </Toolbar>
+      </AppBar>
+
+      {/* main content */}
+      <div className={classes.page}>
+        <div className={classes.toolbar}></div>
+        {children}
+      </div>
+    </div>
+  );
+}
