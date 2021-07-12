5ca6274caf1b7972e3ad360a1cec67baa75da33f
Abdelmouhaymine Salah <asalah@student.1337.ma>
2021-07-12 18:52:58 +0100

    add man img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
+import { makeStyles } from "@material-ui/core";
 import WhyCard from "../components/WhyCard";
+import Image from "../assets/man.png";
+
+const useStyles = makeStyles((theme) => {
+  return {
+    man: {
+      border: "2px solid #000000",
+      width: "100%",
+      verticalAlign: "top",
+      transform: "rotate(5deg)",
+    },
+  };
+});
 
 const Home = () => {
+  const classes = useStyles();
   return (
     <div>
       <WhyCard />
+      <img src={Image} alt="man" className={classes.man} />
     </div>
   );
 };
