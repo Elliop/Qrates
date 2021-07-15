c002bbdea41bfcf67df894ac606bdd0b4e3d2716
Abdelmouhaymine Salah <asalah@student.1337.ma>
2021-07-15 13:18:26 +0100

    cont Card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,6 +38,21 @@ const useStyles = makeStyles((theme) => {
       padding: "110px 0 97px",
       textAlign: "center",
     },
+    text1: {
+      fontSize: "20px",
+    },
+    text2: {
+      paddingTop: "10px",
+      fontSize: "16px",
+      margin: "0 0 0 15%",
+      cursor: "pointer",
+      textDecoration: "underline",
+    },
+    cont: {
+      color: "white",
+      backgroundColor: "black",
+      padding: "72px 8% 85px",
+    },
   };
 });
 
@@ -71,6 +86,19 @@ const Home = () => {
         </Grid>
         <Grid item xs={6}>
           <PressSell />
+        </Grid>
+        <Grid container className={classes.cont}>
+          <Grid item xs={8}>
+            <Typography variant="h5" className={classes.text1}>
+              Just after straight up vinyl pressing? We do that too. Records are
+              shipped in bulk to wherever you need.
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography className={classes.text2} variant="h5">
+              START A PRESS ONLY PROJECT
+            </Typography>
+          </Grid>
         </Grid>
       </Grid>
     </div>
