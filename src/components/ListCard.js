92a4ea90aaef528f6baa7778b3b59d63a96790a8
Abdelmouhaymine Salah <asalah@student.1337.ma>
2021-07-20 16:45:56 +0100

    fix list (Horizontal Ticker)

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -3,24 +3,7 @@ import { makeStyles, Typography } from "@material-ui/core";
 const useStyles = makeStyles((theme) => {
   return {
     root: {
-      width: "100%",
-      padding: "6em 0",
-    },
-    wrap: {
-      paddingLeft: "100%",
-    },
-    move: {
-      display: "inline-block",
-      whiteSpace: "nowrap",
-      paddingRight: "100%",
-      animationIterationCount: "infinite",
-      animationTimingFunction: "linear",
-      animationName: "ticker",
-      animationDuration: "30s",
-    },
-    item: {
-      display: "inline-block",
-      padding: "0 2em",
+      padding: "3em 0 7em",
     },
   };
 });
@@ -28,36 +11,20 @@ const useStyles = makeStyles((theme) => {
 const ListCard = () => {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <div className={classes.wrap}>
-        <div className={classes.move}>
-          <div className={classes.item}>
-            <Typography>CHILLED COW IS ON QRATES</Typography>
-          </div>
-          <div className={classes.item}>
-            <Typography>SOL IS ON QRATES</Typography>
-          </div>
-          <div className={classes.item}>
-            <Typography>VUFLPECK IS ON QRATES</Typography>
-          </div>
-          <div className={classes.item}>
-            <Typography>CHILLHOP IS ON QRATES</Typography>
-          </div>
-          <div className={classes.item}>
-            <Typography>CHILLED COW IS ON QRATES</Typography>
-          </div>
-          <div className={classes.item}>
-            <Typography>SOL IS ON QRATES</Typography>
-          </div>
-          <div className={classes.item}>
-            <Typography>VUFLPECK IS ON QRATES</Typography>
-          </div>
-          <div className={classes.item}>
-            <Typography>CHILLHOP IS ON QRATES</Typography>
-          </div>
-        </div>
+   <div className={classes.root}>
+    <div class="ticker-wrap">
+      <div class="ticker">
+        <div class="ticker__item"><Typography>CHILLED COW IS ON QRATES</Typography></div>
+        <div class="ticker__item"><Typography>SOL IS ON QRATES</Typography></div>
+        <div class="ticker__item"><Typography>VUFLPECK IS ON QRATES</Typography></div>
+        <div class="ticker__item"> <Typography>CHILLHOP IS ON QRATES</Typography></div>
+        <div class="ticker__item"><Typography>CHILLED COW IS ON QRATES</Typography></div>
+        <div class="ticker__item"><Typography>SOL IS ON QRATES</Typography></div>
+        <div class="ticker__item"><Typography>VUFLPECK IS ON QRATES</Typography></div>
+        <div class="ticker__item"> <Typography>CHILLHOP IS ON QRATES</Typography></div>
       </div>
     </div>
+   </div>
   );
 };
 
