ad76abc02a2ae04734454610927f43e0f960c1ed
Abdelmouhaymine Salah <asalah@student.1337.ma>
2021-07-20 15:47:03 +0100

    horizontal ticker

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,17 +1,64 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => {
   return {
     root: {
-      flexGrow: 1,
-      marginTop: "2em",
+      width: "100%",
+      padding: "6em 0",
+    },
+    wrap: {
+      paddingLeft: "100%",
+    },
+    move: {
+      display: "inline-block",
+      whiteSpace: "nowrap",
+      paddingRight: "100%",
+      animationIterationCount: "infinite",
+      animationTimingFunction: "linear",
+      animationName: "ticker",
+      animationDuration: "30s",
+    },
+    item: {
+      display: "inline-block",
+      padding: "0 2em",
     },
   };
 });
 
 const ListCard = () => {
   const classes = useStyles();
-  return <div className={classes.root}>List</div>;
+  return (
+    <div className={classes.root}>
+      <div className={classes.wrap}>
+        <div className={classes.move}>
+          <div className={classes.item}>
+            <Typography>CHILLED COW IS ON QRATES</Typography>
+          </div>
+          <div className={classes.item}>
+            <Typography>SOL IS ON QRATES</Typography>
+          </div>
+          <div className={classes.item}>
+            <Typography>VUFLPECK IS ON QRATES</Typography>
+          </div>
+          <div className={classes.item}>
+            <Typography>CHILLHOP IS ON QRATES</Typography>
+          </div>
+          <div className={classes.item}>
+            <Typography>CHILLED COW IS ON QRATES</Typography>
+          </div>
+          <div className={classes.item}>
+            <Typography>SOL IS ON QRATES</Typography>
+          </div>
+          <div className={classes.item}>
+            <Typography>VUFLPECK IS ON QRATES</Typography>
+          </div>
+          <div className={classes.item}>
+            <Typography>CHILLHOP IS ON QRATES</Typography>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListCard;
